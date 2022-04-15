0b4884f6ba47d1150768a6e43ac2b6c9435ca5a5
Hyeonjun <tjguswns0509@naver.com>
2022-04-15 19:52:20 +0900

    서버 작업 1주차

--- src/main/java/Controller/TotalRestController.java
+++ src/main/java/Controller/TotalRestController.java
@@ -1,9 +1,9 @@
 package Controller;
 
 import Class.*;
-import Dto.PersonalDto;
-import Repository.PersonalRepository;
-import Service.PersonalService;
+import Dto.*;
+import Repository.*;
+import Service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,29 +13,113 @@ import java.util.List;
 @RestController
 public class TotalRestController {
 
-    private final PersonalRepository personalRepository;
+    private final CafeRepository cafeRepository;
 
-    private final PersonalService personalService;
+    private final CafeService cafeService;
 
-    @GetMapping("/personal")
-    public List<Personal> getPersonal() {
-        return personalRepository.findAll();
+    @GetMapping("/cafe") // create
+    public List<Cafe> getCafe() {
+        return cafeRepository.findAll();
     }
 
-    @PostMapping("/personal")
-    public Personal createPersonal(@RequestBody PersonalDto personalDto) {
-        Personal personal = new Personal(personalDto);
-        return personalRepository.save(personal);
+    @PostMapping("/cafe") // read
+    public Cafe createCafe(@RequestBody CafeDto cafeDto) {
+        Cafe cafe = new Cafe(cafeDto);
+        return cafeRepository.save(cafe);
     }
 
-    @PutMapping("/personal/{mem_num}")
-    public Long updatePersonal(@PathVariable Long mem_num, @RequestBody PersonalDto personalDto) {
-        return personalService.update(mem_num, personalDto);
+    @PutMapping("/cafe/{cafeNum}") // update
+    public Long updateCafe(@PathVariable Long cafeNum, @RequestBody CafeDto cafeDto) {
+        return cafeService.update(cafeNum, cafeDto);
     }
 
-    @DeleteMapping("/personal/{mem_num}")
-    public Long deletePersonal(@PathVariable Long mem_num) {
-        personalRepository.deleteById(mem_num);
-        return mem_num;
+    @DeleteMapping("/cafe/{cafeNum}") // delete
+    public Long deleteCafe(@PathVariable Long cafeNum) {
+        cafeRepository.deleteById(cafeNum);
+        return cafeNum;
+    }
+
+
+
+    private final BookmarkRepository bookmarkRepository;
+
+    private final BookmarkService bookmarkService;
+
+    @GetMapping("/bookmark") // create
+    public List<Bookmark> getBookmark() {
+        return bookmarkRepository.findAll();
+    }
+
+    @PostMapping("/bookmark") // read
+    public Bookmark createBookmark(@RequestBody BookmarkDto bookmarkDto) {
+        Bookmark bookmark = new Bookmark(bookmarkDto);
+        return bookmarkRepository.save(bookmark);
+    }
+
+    @PutMapping("/bookmark/{bookmarkNum}") // update
+    public Long updateBookmark(@PathVariable Long bookmarkNum, @RequestBody BookmarkDto bookmarkDto) {
+        return bookmarkService.update(bookmarkNum, bookmarkDto);
+    }
+
+    @DeleteMapping("/bookmark/{bookmarkNum}") // delete
+    public Long deleteBookmark(@PathVariable Long bookmarkNum) {
+        bookmarkRepository.deleteById(bookmarkNum);
+        return bookmarkNum;
+    }
+
+
+
+    private final CategoryRepository categoryRepository;
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/category") // create
+    public List<category> getCategory() {
+        return categoryRepository.findAll();
+    }
+
+    @PostMapping("/category") // read
+    public Category createCategory(@RequestBody CategoryDto categoryDto) {
+        Category category = new Category(categoryDto);
+        return categoryRepository.save(category);
+    }
+
+    @PutMapping("/category/{categoryNum}") // update
+    public Long updateCategory(@PathVariable Long categoryNum, @RequestBody CategoryDto categoryDto) {
+        return categoryService.update(categoryNum, categoryDto);
+    }
+
+    @DeleteMapping("/category/{categoryNum}") // delete
+    public Long deleteCategory(@PathVariable Long categoryNum) {
+        categoryRepository.deleteById(categoryNum);
+        return categoryNum;
+    }
+
+
+
+    private final ReviewRepository reviewRepository;
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/review") // create
+    public List<review> getReview() {
+        return reviewRepository.findAll();
+    }
+
+    @PostMapping("/review") // read
+    public Review createReview(@RequestBody ReviewDto reviewDto) {
+        Review review = new Review(reviewDto);
+        return reviewRepository.save(review);
+    }
+
+    @PutMapping("/review/{reviewNum}") // update
+    public Long updateReview(@PathVariable Long reviewNum, @RequestBody ReviewDto reviewDto) {
+        return reviewService.update(reviewNum, reviewDto);
+    }
+
+    @DeleteMapping("/review/{reviewNum}") // delete
+    public Long deleteReview(@PathVariable Long reviewNum) {
+        reviewRepository.deleteById(reviewNum);
+        return reviewNum;
     }
 }
--- src/main/java/Repository/PersonalRepository.java
+++
@@ -1,7 +0,0 @@
-package Repository;
-
-import Class.Personal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonalRepository extends JpaRepository<Personal, Long> {
-}
